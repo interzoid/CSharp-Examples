6a6d32165230f48e2e410c4dd2e92891fe9f4561
Interzoid <50465512+interzoid@users.noreply.github.com>
2021-09-13 15:00:21 -0700

    Create GetAddressMatchSimKey.cs

---
+++ GetAddressMatchSimKey.cs
@@ -0,0 +1,27 @@
+// Get your API key at www.interzoid.com/register
+using System;
+using System.Net.Http;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://api.interzoid.com/getaddressmatchadvanced?license=YOURAPIKEY&address=500 main street suite 101&algorithm=narrow"),
+                Headers =
+            {
+                { "x-api-key", "YOUR-API-KEY" },
+            },
+            };
+            using var response = client.Send(request);
+            {
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+        }
+    }
+}
