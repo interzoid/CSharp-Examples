cbd233741d4d7786e0da6eaea99a38fcb8205107
Interzoid <50465512+interzoid@users.noreply.github.com>
2021-09-13 12:05:24 -0700

    Create GetCompanyMatchSimKey.cs

---
+++ GetCompanyMatchSimKey.cs
@@ -0,0 +1,27 @@
+// Get your API key at www.interzoid.com/register
+using System;
+using System.Net.Http;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://api.interzoid.com/getcompanymatchadvanced?company=IBM&algorithm=wide"),
+                Headers =
+            {
+                { "x-api-key", "YOUR-API-KEY" },
+            },
+            };
+            using var response = client.Send(request);
+            {
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+        }
+    }
+}
