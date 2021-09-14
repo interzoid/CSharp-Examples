1895399557122387d724e31cd3c4fe11557ed5f4
Interzoid <50465512+interzoid@users.noreply.github.com>
2021-09-13 17:07:46 -0700

    Create GetCurrencyRate.cs

---
+++ GetCurrencyRate.cs
@@ -0,0 +1,28 @@
+
+// Get your API key at www.interzoid.com/register
+using System;
+using System.Net.Http;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://api.interzoid.com/getcurrencyrate?symbol=EUR"),
+                Headers =
+            {
+                { "x-api-key", "YOUR-API-KEY" },
+            },
+            };
+            using var response = client.Send(request);
+            {
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+        }
+    }
+}
